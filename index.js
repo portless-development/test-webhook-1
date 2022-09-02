d5347213797a3e400b316184eb1954f25a8037ab
drscripts <nathanael.vd@gmail.com>
2022-09-02 10:16:11 +0700

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000 
 
 app.get('/', (req, res) => {
-  res.send('Hello awdawdawdawdawd!')
+  res.send('Hello awdawdawdawdawd asdawd!')
 })
 
 app.listen(port, () => {
